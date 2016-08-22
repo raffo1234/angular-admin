2f81928d9f36df844023e7f6595ac1a516938d17
raffo1234 <ralphch1234562@hotmail.com>
2016-08-22 00:08:52 -0500

    Update

--- js/constant.js
+++ js/constant.js
@@ -2,8 +2,8 @@ materialAdmin
 
 .constant("API_URL", {
 	// PROD
-	// 'url': "http://igospa.dhdinc.info/api/v1/"
+	'url': "http://igospa.dhdinc.info/api/v1/"
 
 	// LOCALHOST
-	'url': "http://localhost:888/igospa/api/v1/"
+	// 'url': "http://localhost:888/igospa/api/v1/"
 })
